782c6db9ba13827fa14020538530b21b749c6254
ishaqmdgcp <55760349+ishaqmdgcp@users.noreply.github.com>
2020-08-29 09:53:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+# Configure the Google Cloud provider
+provider "google" {
+  project = "ishaqgcpproject"
+  region  = "us-central1"
+}
+
+# Create a Google Compute instance
+resource "google_compute_instance" "example" {
+  name          = "terraform"
+  machine_type  = "f1-micro"
+  zone          = "us-central1-a"
+  #tags = ["prod","web"]
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
